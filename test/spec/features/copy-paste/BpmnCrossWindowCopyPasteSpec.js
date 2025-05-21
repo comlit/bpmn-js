dad8a968a507ffa84d05fc63d6056d48e66a6dfb
MJW97 <MatthiasJamesWalsh97@gmail.com>
2025-05-21 16:40:36 +0200

    Worked out basic test ideas

---
+++ test/spec/features/copy-paste/BpmnCrossWindowCopyPasteSpec.js
@@ -0,0 +1,17 @@
+//Test Event catch for focus changee
+
+import EventBus from "diagram-js/lib/core/EventBus";
+
+//Trigger Event of focus change: should exist in event handler tests
+eventBus.fire('canvas.focus.changed', {focused: true})
+//Check if caught
+
+//Reviver
+//False JSON
+//Valid JSON
+
+//Test copy into system cliboard
+//Mock data into cliboard
+//Mockdata out of clipboard
+//check against xpcted output
+
