1e6bf41b1ff7d48e100b960406b33e81a9aacb04
MJW97 <MatthiasJamesWalsh97@gmail.com>
2025-05-27 13:09:30 +0200

    Clean CrossWindowCopy of old log statements. Implented initial testing structure.
    There was an uncaught error when parsing a non JSON object, this is now caught. Foundational structure for the testing suite is implemented. Still requires invalid JSON check and catch focus event check.

--- test/spec/features/copy-paste/BpmnCrossWindowCopyPasteSpec.js
+++ test/spec/features/copy-paste/BpmnCrossWindowCopyPasteSpec.js
@@ -1,17 +1,73 @@
-//Test Event catch for focus changee
+import {
+  inject
+} from 'test/TestHelper';
 
-import EventBus from "diagram-js/lib/core/EventBus";
+// import EventBus from 'diagram-js/lib/core/EventBus';
+// import BpmnCrossWindowCopyPaste from 'lib/features/copy-paste/BpmnCrossWindowCopyPaste';
 
-//Trigger Event of focus change: should exist in event handler tests
-eventBus.fire('canvas.focus.changed', {focused: true})
-//Check if caught
 
-//Reviver
-//False JSON
-//Valid JSON
+describe('features/window-copy-paste', function() {
 
-//Test copy into system cliboard
-//Mock data into cliboard
-//Mockdata out of clipboard
-//check against xpcted output
+  // Test Reviver
+  describe('revive string', function() {
+
+    // False JSON
+    it('should pass with valid JSON', inject(function(elementRegistry, BpmnCrossWindowCopyPaste, moddle) {
+
+      // create event
+      var startEvent = elementRegistry.get('StartEvent_1'),
+          mockString = JSON.stringify(startEvent);
+
+      // revive mock string
+      var revivedEvent = JSON.parse(mockString, BpmnCrossWindowCopyPaste.createReviver(moddle));
+
+      // checks that system copy is identical to the input string.
+      expect(revivedEvent).to.equal(startEvent);
+    }));
+
+    // Invalid JSON
+    it('should fail with invalid JSON', inject(function() {
+
+      // create valid JSON
+
+      // Check valid return
+    }));
+
+  });
+
+  // Test copy/paste into system cliboard
+  describe('copy/paste into system clipboard', function() {
+
+    // Mock data into cliboard
+    it('should copy into system clipboard', inject(function(elementRegistry, copyPaste) {
+
+      // create mock JSON string
+      var startEvent = elementRegistry.get('StartEvent_1'),
+          mockString = JSON.stringify(startEvent);
+
+      // triggers copy event, which triggers system copy.
+      copyPaste.copy(startEvent);
+
+      // checks that system copy is identical to the input string.
+      navigator.clipboard.readText().then(text => { expect(text).to.equal(mockString); });
+    }));
+
+  });
+
+  /*
+  // Test Event catch for focus changee
+  describe('focus change', function() {
+
+    it('should catch focus change event', inject(function() {
+
+      // Trigger Event of focus change
+      EventBus.fire('canvas.focus.changed', { focused: true });
+
+      // Check if caught
+    }));
+
+  });
+  */
+
+});
 
